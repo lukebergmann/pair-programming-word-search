c6d8d9ec77e1e06a2833de5c7f5fabfb551297e7
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-11 07:49:12 +0000

    4/4 tests passing

--- wordsearch.js
+++ wordsearch.js
@@ -1,11 +1,13 @@
-const transpose = function (matrix) {
-  let a = matrix.length; // rows
-  let b = matrix[0].length; // columns
+// Pair Programming completed by Eric Guthrie, Amanda Tuyen, and Luke Bergmann
+
+const transpose = function (letters) {
+  let a = letters.length; // rows
+  let b = letters[0].length; // columns
   let newArr = [];
   for (let i = 0; i < b; i++) {
     newArr[i] = [];
     for (let j = 0; j < a; j++) {
-      newArr[i][j] = matrix[j][i];
+      newArr[i][j] = letters[j][i];
     }
   }
   return newArr;
@@ -14,11 +16,15 @@ const transpose = function (matrix) {
 
 
 const wordSearch = (letters, word) => {
+    if(letters.length === 0) {
+        return false;
+    }
+
   const horizontalJoin = letters.map(ls => ls.join(''));
+
   let verticalArr = transpose(letters);
-  console.log('array', verticalArr);
+
   const verticalJoin = verticalArr.map(ls => ls.join(''));
-  console.log('vertJoin', verticalJoin);
   
   for (let l of horizontalJoin) {
     if (l.includes(word)) {
@@ -28,12 +34,9 @@ const wordSearch = (letters, word) => {
   for (let l of verticalJoin) {
     if (l.includes(word)) {
       return true;
-      
     }
   }
   return false;
 }
 
-
 module.exports = wordSearch
-
