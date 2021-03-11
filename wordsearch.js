d93767e851c724c80857d4d8eedd28cded6c3e12
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-11 07:01:04 +0000

    3/4 test passing

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+  let a = matrix.length; // rows
+  let b = matrix[0].length; // columns
+  let newArr = [];
+  for (let i = 0; i < b; i++) {
+    newArr[i] = [];
+    for (let j = 0; j < a; j++) {
+      newArr[i][j] = matrix[j][i];
     }
+  }
+  return newArr;
+};
+
+
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let verticalArr = transpose(letters);
+  console.log('array', verticalArr);
+  const verticalJoin = verticalArr.map(ls => ls.join(''));
+  console.log('vertJoin', verticalJoin);
+  
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+      
+    }
+  }
+  return false;
 }
 
+
 module.exports = wordSearch
+
